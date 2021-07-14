9030e3205f58f1f1ff9dd08273a1a9395c48eb76
xiangyuzi <ayuko.weiji@gmail.com>
2021-07-14 18:19:34 +0900

    新規登録、ログアウト機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :lastname, presence: true
+  validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_reading, presence: true
   validates :first_name_reading, presence: true
