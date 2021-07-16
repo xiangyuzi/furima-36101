68bffdc520c1962fedb3c8b19c27189d578be88c
xiangyuzi <ayuko.weiji@gmail.com>
2021-07-16 14:44:59 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_reading, presence: true
-  validates :first_name_reading, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invaild. Include both letters and numbers'
   validates :birth_date, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invaild. Input full-width characters' } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ンー－]+\z/, message: 'is invaild. Input full-width katakana characters' } do
+    validates :last_name_reading
+    validates :first_name_reading
+  end
 end
