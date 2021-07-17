523717fdb268939204b848b7abbab34a7de78d17
xiangyuzi <ayuko.weiji@gmail.com>
2021-07-17 12:12:01 +0900

    Basic認証を導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -11,6 +11,6 @@ FactoryBot.define do
     first_name            {person.first.kanji}
     last_name_reading     {person.last.katakana}
     first_name_reading    {person.first.katakana}
-    birth_date            {Faker::Date.between(from: '1930-01-01', to: '2021-12-31')}
+    birth_date            {Faker::Date.between(from: '1930-01-01', to: Date.today )}
   end
 end
