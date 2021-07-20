3769d25fab6745c2b6f77f8877b7d0fe8abdbffb
xiangyuzi <ayuko.weiji@gmail.com>
2021-07-20 12:29:56 +0900

    一覧表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    @items = Item.includes(:user)
+    @items = Item.includes(:user).order(id: "DESC")
   end
 
   def new
