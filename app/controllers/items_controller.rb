a048a015de4355a7b4cde38743563cda1610508d
xiangyuzi <ayuko.weiji@gmail.com>
2021-07-20 23:32:36 +0900

    閲覧制限実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.includes(:user).order(id: 'DESC')
@@ -36,9 +37,14 @@ class ItemsController < ApplicationController
   end
 
   private
-
   def item_params
     params.require(:item).permit(:title, :detail, :category_id, :condition_id, :handling_payer_id, :prefecture_id, :lead_time_id,
                                  :price, :image).merge(user_id: current_user.id)
+  end
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless @item.user.id == current_user.id
+      redirect_to action: :index
+    end
   end
 end
