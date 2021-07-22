452bea2f494dc44ec8fb1838a94ad927fa8d5faa
xiangyuzi <ayuko.weiji@gmail.com>
2021-07-22 14:48:17 +0900

    createアクション実装

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,20 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_number, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message:"is invalid. Enter it as follows(e.g. 123-4567)" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: /\A[0-9]{10}+\z/, message: "is too short"}
+    validates :user_id
+    validates :item_id
+  end
+  validates :phone_number, numericality: {only_integer: true,  message: "is invalid. Input only number" }
+  
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_number: building_number, phone_number: phone_number, purchase: purchase.id)
+  end
+end
---
+++ spec/models/address_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe Address, type: :model do
+  before do
+    @
+end
