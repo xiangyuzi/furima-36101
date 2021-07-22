9d24ff8e46ccf8960664bdd7fdf10d7b3770b69d
xiangyuzi <ayuko.weiji@gmail.com>
2021-07-22 18:23:13 +0900

    テストコード実装

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -7,7 +7,7 @@ class PurchaseAddress
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
     validates :city
     validates :house_number
-    validates :phone_number, format: { with: /\A[0-9]{10}+\z/, message: "is too short"}
+    validates :phone_number, format: { with: /\A[0-9]{10}+\z|\A[0-9]{11}+\z/, message: "is too short"}
     validates :user_id
     validates :item_id
   end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  before do
-    @
+  pending "add some examples to (or delete) #{__FILE__}"
 end
